3844c4f45b366364058efdb835a994cd5ad1c261
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2023-06-25 21:37:02 -0500

    name page

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ function NavBar() {
     return (
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
 
-            <a className="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+            <a className="navbar-brand ps-3" href="/">Movilidad Escolar</a>
 
             <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars"></i></button>
 
